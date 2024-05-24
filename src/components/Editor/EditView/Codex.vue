ceeaa0d0ced7ec44bf576a75033cd5abc64a7e1a
Crunchitect <95293374+Crunchitect@users.noreply.github.com>
2024-05-24 17:56:58 +0700

    code mir

--- src/components/Editor/EditView/Codex.vue
+++ src/components/Editor/EditView/Codex.vue
@@ -1,10 +1,21 @@
 <script lang="ts" setup>
     import { generatedCode } from '@/state/Editor';
+    import { Codemirror } from 'vue-codemirror';
+    import { cpp } from '@codemirror/lang-cpp';
+    import { oneDarkTheme } from '@codemirror/theme-one-dark';
 </script>
 
 
 <template>
     <div class="w-full h-[95%] bg-slate-100 rounded flex flex-wrap place-content-center">
-        <pre class="w-4/5 h-4/5 rounded overflow-auto bg-slate-900 text-white p-3">{{ generatedCode }}</pre>
+        <codemirror
+            v-model="generatedCode"
+            placeholder="Code goes here..."
+            :style="{ height: '80%', width: '80%', 'border-radius': '4px' }"
+            :autofocus="true"
+            :indent-with-tab="true"
+            :tab-size="4"
+            :extensions="[cpp(), oneDarkTheme]"
+        />
     </div>
 </template>
