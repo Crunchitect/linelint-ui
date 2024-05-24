5ddb565c4acf0e050d4c79609cf7b700a1abc775
PremPremPremPrem <prem.sy1952@gmail.com>
2024-05-24 21:01:13 +0700

    code exporting

--- src/components/Editor/EditView/Codex.vue
+++ src/components/Editor/EditView/Codex.vue
@@ -2,20 +2,39 @@
     import { generatedCode } from '@/state/Editor';
     import { Codemirror } from 'vue-codemirror';
     import { cpp } from '@codemirror/lang-cpp';
-    import { oneDarkTheme } from '@codemirror/theme-one-dark';
+    import { birdsOfParadise } from 'thememirror';
+
+    const copy = () => {
+        navigator.clipboard.writeText(generatedCode.value);
+    }
+
+    const cRobot = () => {
+        var save = document.createElement("a");
+        save.setAttribute("href", "data:text/plain;charset=umenttttf-8," + encodeURI(generatedCode.value));
+        save.setAttribute("download", "code.c");
+
+        document.body.appendChild(save);
+        save.click();
+        document.body.removeChild(save);
+    }
+    
 </script>
 
 
 <template>
-    <div class="w-full h-[95%] bg-slate-100 rounded flex flex-wrap place-content-center">
-        <codemirror
+    <div class="w-full h-[95%] bg-slate-100 rounded flex flex-col flex-wrap place-content-center gap-2">
+        <Codemirror
             v-model="generatedCode"
-            placeholder="Code goes here..."
+            placeholder="Wait for code to generate..."
             :style="{ height: '80%', width: '80%', 'border-radius': '4px' }"
             :autofocus="true"
             :indent-with-tab="true"
             :tab-size="4"
-            :extensions="[cpp(), oneDarkTheme]"
+            :extensions="[cpp(), birdsOfParadise]"
         />
+        <div class="flex w-4/5 justify-between">
+            <button class="bg-slate-200 w-2/5 p-4 rounded text-xl active:bg-slate-300" @click="copy"><i class="fa-solid fa-copy"></i> Copy</button>
+            <button class="bg-slate-200 w-2/5 p-4 rounded text-xl active:bg-slate-300" @click="cRobot"><i class="fa-solid fa-download flex-grow"></i> Export to C Robot</button>
+        </div>
     </div>
 </template>
