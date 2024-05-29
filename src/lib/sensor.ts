592ff0ac2e8f4b39e2864a8d113afaf443f4df10
Crunchitect <95293374+Crunchitect@users.noreply.github.com>
2024-05-29 15:25:58 +0700

    Final Software Update

--- src/lib/sensor.ts
+++ src/lib/sensor.ts
@@ -1,19 +1,27 @@
 import { sensorData } from "@/state/Editor";
 
-export function calcSensor(refs: {[k: string]: HTMLElement | HTMLElement[]}, scale: [number, number]) {
+export function calcSensor(
+    refs: { [k: string]: HTMLElement | HTMLElement[] },
+    scale: [number, number]
+) {
     const sensorLocations = [];
-    
-    const canvasEl = (<HTMLElement>refs[`sensorCanvas`]);
+
+    const canvasEl = <HTMLElement>refs[`sensorCanvas`];
     const canvasBBox = canvasEl.getBoundingClientRect();
-    
-    const originEl = (<HTMLElement>refs[`robotOrigin`]);
-    const { width: originWidth, height: originHeight } = originEl.getBoundingClientRect();
+
+    const originEl = <HTMLElement>refs[`robotOrigin`];
+    const { width: originWidth, height: originHeight } =
+        originEl.getBoundingClientRect();
     const originCSSMatrix = getComputedStyle(originEl).transform;
     let originX, originY, originR;
-    if (originCSSMatrix == 'none') {
-        originX = 0; originY = 0; originR = 0;
+    if (originCSSMatrix == "none") {
+        originX = 0;
+        originY = 0;
+        originR = 0;
     } else {
-        const originMatrix = originCSSMatrix.match(/-?\d+\.?\d*/g)?.map(val => parseFloat(val))!;
+        const originMatrix = originCSSMatrix
+            .match(/-?\d+\.?\d*/g)
+            ?.map((val) => parseFloat(val))!;
         originX = originMatrix[4] + originWidth / 2;
         originY = originMatrix[5] + originHeight / 2;
         originR = Math.atan2(originMatrix[1], originMatrix[0]);
@@ -21,24 +29,40 @@ export function calcSensor(refs: {[k: string]: HTMLElement | HTMLElement[]}, sca
 
     for (let i = 0; i < 5; i++) {
         const sensorEl = (<HTMLElement[]>refs[`sensor-${i}`])[0];
-        const { width: sensorWidth, height: sensorHeight } = sensorEl.getBoundingClientRect();
+        const { width: sensorWidth, height: sensorHeight } =
+            sensorEl.getBoundingClientRect();
         const sensorCSSMatrix = getComputedStyle(sensorEl).transform;
         let sensorX, sensorY, sensorR;
-        if (sensorCSSMatrix == 'none') {
-            sensorX = 0; sensorY = 0; sensorR = 0;
+        if (sensorCSSMatrix == "none") {
+            sensorX = 0;
+            sensorY = 0;
+            sensorR = 0;
         } else {
-            const sensorMatrix = sensorCSSMatrix.match(/-?\d+\.?\d*/g)?.map(val => parseFloat(val))!;
+            const sensorMatrix = sensorCSSMatrix
+                .match(/-?\d+\.?\d*/g)
+                ?.map((val) => parseFloat(val))!;
             sensorX = sensorMatrix[4] + sensorWidth / 2;
             sensorY = sensorMatrix[5] + sensorHeight / 2;
             sensorR = Math.atan2(sensorMatrix[1], sensorMatrix[0]);
         }
-        
-        const sensorRotatedX = (Math.cos(-originR) * (sensorX - originX)) - (Math.sin(-originR) * (sensorY - originY));
-        const sensorRotatedY = (Math.sin(-originR) * (sensorX - originX)) + (Math.cos(-originR) * (sensorY - originY));
-        
-        const sensorNormalizedX = Math.floor(sensorRotatedX / canvasBBox.width * scale[0]  / 10) * 10;
-        const sensorNormalizedY = Math.floor(sensorRotatedY / canvasBBox.height * scale[1] / 10) * 10;
-        sensorLocations.push(<[number, number]>[sensorNormalizedX, sensorNormalizedY]);
+
+        const sensorRotatedX =
+            Math.cos(-originR) * (sensorX - originX) -
+            Math.sin(-originR) * (sensorY - originY);
+        const sensorRotatedY =
+            Math.sin(-originR) * (sensorX - originX) +
+            Math.cos(-originR) * (sensorY - originY);
+
+        const sensorNormalizedX =
+            Math.floor(((sensorRotatedX / canvasBBox.width) * scale[0]) / 10) *
+            10;
+        const sensorNormalizedY =
+            Math.floor(((sensorRotatedY / canvasBBox.height) * scale[1]) / 10) *
+            10;
+        sensorLocations.push(<[number, number]>[
+            sensorNormalizedX,
+            sensorNormalizedY,
+        ]);
     }
     sensorData.value = sensorLocations;
     return sensorLocations;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,13 +2,19 @@ export type Image = [number, number, number, number][][];
 export type BinaryImage = number[][];
 
 export enum Tabs {
-    RobotMap = 'map',
-    // Sensors = 'screwdriver-wrench',
-    Junctions = 'code-fork',
-    Options = 'gear',
-    Code = 'code'
+    RobotMap = "map",
+    Sensors = 'screwdriver-wrench',
+    Junctions = "code-fork",
+    // Options = "gear",
+    Code = "code",
 }
 
 export type Vec2D = [number, number];
 
-export type FixedLengthArray<Type, Length extends number, Temp extends readonly Type[] = []> = Length extends Temp['length'] ? Temp : FixedLengthArray<Type, Length, [Type, ...Temp]>;
+export type FixedLengthArray<
+    Type,
+    Length extends number,
+    Temp extends readonly Type[] = []
+> = Length extends Temp["length"]
+    ? Temp
+    : FixedLengthArray<Type, Length, [Type, ...Temp]>;
